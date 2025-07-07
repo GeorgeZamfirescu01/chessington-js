5546bd8c320895c0b500e1178bb7db1166c590e2
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-07 12:50:14 +0300

    Fixed out of bounds issues for pawns. Are they not supposed to change on the final square though?

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -16,9 +16,13 @@ export default class Pawn extends Piece {
         const squareForward = new Square(position.row + direction, position.col);
         const squareTwoForward = new Square(position.row + 2 * direction, position.col);
 
-        if (board.getPiece(squareForward) === undefined) {
+        function isIn(row: Number, col: Number) {
+            return row >= 0 && col >= 0 && row < 8 && col < 8;
+        }
+
+        if (isIn(squareForward.row, squareForward.col) && board.getPiece(squareForward) === undefined) {
             availableMoves.push(new Square(position.row + direction, position.col));
-            if (board.getPiece(squareTwoForward) === undefined) {
+            if (isIn(squareTwoForward.row, squareTwoForward.col) && board.getPiece(squareTwoForward) === undefined) {
                 if (this.player === Player.WHITE && position.row === 1 || this.player === Player.BLACK && position.row === 6) {
                     availableMoves.push(squareTwoForward);
                 }
