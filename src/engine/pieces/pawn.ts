81e20ae8c5123b04d735add72909099152e071ae
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-07 10:58:54 +0300

    Made the pawns be able to move two squares when in starting positions, and one otherwise; did not take care of the edge case of being at the end

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position = board.findPiece(this);
+        const direction = this.player === Player.WHITE ? 1 : -1;
+        const availableMoves = [];
+
+
+        availableMoves.push(new Square(position.row + direction, position.col));
+        if (this.player === Player.WHITE && position.row === 1 || this.player === Player.BLACK && position.row === 7) {
+            availableMoves.push(new Square(position.row + 2 * direction, position.col));
+        }
+
+        return availableMoves;
     }
 }
