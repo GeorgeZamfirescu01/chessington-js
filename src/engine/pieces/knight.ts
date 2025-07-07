f4b8db54ddb613ee76d3feb643ccc1aceb3813cc
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-07 11:49:45 +0300

    Implemented knight functionality

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,29 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        function isIn(row: number, col: number) {
+            return row >= 0 && col >= 0 && row < 8 && col < 8;
+        }
+
+        const options = [[1, 1], [-1, -1], [-1, 1], [1, -1]];
+        options.forEach(option => {
+            const [rowSign, colSign] = option;
+
+            let newRow = position.row + 2 * rowSign;
+            let newCol = position.col + 1 * colSign;
+            if (isIn(newRow, newCol)) {
+                availableMoves.push(new Square(newRow, newCol));
+            }
+            newRow = position.row + 1 * rowSign;
+            newCol = position.col + 2 * colSign;
+            if (isIn(newRow, newCol)) {
+                availableMoves.push(new Square(newRow, newCol));
+            }
+        });
+
+        return availableMoves;
     }
 }
