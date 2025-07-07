2a501ce826102a182d030752a55804b086c2e5a7
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-07 11:37:50 +0300

    Implemented queen functionality (by copying rook and bishop code)

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Rook from "./rook";
+import Square from "../square";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,35 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position = board.findPiece(this);
+        const availableMoves = [];
+        const BOARD_SIDE = 8;
+
+        function isIn(idx: number): boolean {
+            return idx >= 0 && idx < 8;
+        }
+
+        for (let i = 0; i < BOARD_SIDE; i++) {
+            if (i != position.row) {
+                const dif = i - position.row;
+                if (isIn(position.col + dif)) {
+                    availableMoves.push(new Square(i, position.col + dif));
+                }
+                if (isIn(position.col - dif)) {
+                    availableMoves.push(new Square(i, position.col - dif));
+                }
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            if (i != position.row) {
+                availableMoves.push(new Square(i, position.col));
+            }
+            if (i != position.col) {
+                availableMoves.push(new Square(position.row, i));
+            }
+        }
+
+        return availableMoves;
     }
 }
