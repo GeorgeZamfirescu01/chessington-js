2546920250cadf76efa63ae725c0612fd8e1b77a
George Zamfirescu <George.Zamfirescu@softwire.com>
2025-07-07 11:54:12 +0300

    Added king functionality

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,32 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const position = board.findPiece(this);
+        const availableMoves: Square[] = [];
+
+        function isIn(row: number, col: number) {
+            return row >= 0 && col >= 0 && row < 8 && col < 8;
+        }
+
+        const offsets = [
+            [1, 0],
+            [-1, 0],
+            [0, 1],
+            [0, -1],
+            [1, 1],
+            [-1, -1],
+            [1, -1],
+            [-1, 1],
+        ];
+
+        offsets.forEach(offset => {
+            const [dr, dc] = offset;
+            const newRow = position.row + dr, newCol = position.col + dc;
+            if (isIn(newRow, newCol)) {
+                availableMoves.push(new Square(newRow, newCol));
+            }
+        });
+
+        return availableMoves;
     }
 }
